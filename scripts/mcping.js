09e3ce8562252e53bcf4f9ef03639408a6018796
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-15 10:17:51 -0600

    changes mcping.js to allow for server downtime

--- scripts/mcping.js
+++ scripts/mcping.js
@@ -9,14 +9,14 @@ request.open('GET', 'https://api.mcsrvstat.us/2/vegasmp.hydrostaticcog.org', tru
 
 request.onload = function () {
     var data = JSON.parse(this.response)
-    if (data.online = 'true') {
+    if (data.online) {
         if (!data.players.list) {
             var playerList = 'No Players Online'
         } else {
             var playerList = data.players.list
         }
-        document.getElementById("status-playerList").innerHTML = playerList;
-        document.getElementById("status-isOnline").innerHTML = data.online;
+        document.getElementById("status-playerList").innerHTML = 'Players: ' + playerList;
+        document.getElementById("status-isOnline").innerHTML = 'Server Online';
         document.getElementById("status-playerCount").innerHTML = data.players.online +'/'+ data.players.max;
 
     }
