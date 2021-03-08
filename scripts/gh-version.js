74ded3c48e408b65cf3911c9c3090c6f81caa7b0
hydrostaticcog <hydro@hydrostaticcog.org>
2021-03-07 19:40:41 -0600

    Update gh-version.js

--- scripts/gh-version.js
+++ scripts/gh-version.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://raw.githubusercontent.com/Grick-Heart-Project/Grick-Heart/master/package.json', true)
+request.open('GET', 'https://raw.githubusercontent.com/Grick-Heart-Project/Grick-Heart/master/release.json', true)
 
 request.onload = function () {
     var data = JSON.parse(this.response)
