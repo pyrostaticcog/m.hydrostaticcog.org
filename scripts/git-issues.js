a3d9d9f48bb681cda3c5368c2a8dcf2f9c10c5ae
hydrostaticcog <hydro@hydrostaticcog.org>
2021-03-13 22:14:21 -0600

    Update git-issues.js

--- scripts/git-issues.js
+++ scripts/git-issues.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.github.com/repos/Grick-Heart-Project/Grick-Heart/issues/23', true)
+request.open('GET', 'https://api.github.com/repos/Grick-Heart-Project/Grick-Heart/issues/27', true)
 
 request.onload = function () {
     var data = JSON.parse(this.response)
